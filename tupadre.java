b4254b8770364fc940d066fcd6df1bc6d0b06f3e
David Abellán <116668339+Puchdamont@users.noreply.github.com>
2022-11-02 18:39:27 +0100

    Add files via upload

---
+++ tupadre.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class tupadre {
+    public static void main(String[] args) {
+        Scanner r = new Scanner(System.in);
+        int c=r.nextInt();
+        String s="";
+        String s1="";
+        for (int i=0;i<c;i++){
+            s=r.next();
+            s1=r.next();
+            if (s.equals("Luke") && s1.equals("padre")){
+                System.out.println("TOP SECRET");
+            }else System.out.println(s + ", yo soy tu " + s1);
+        }
+    }
+}
