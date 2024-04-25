e47b26bfd72f8da3b5805234c441bd93d851e6ca
David Abellán Lerma <116668339+K0zaky@users.noreply.github.com>
2024-04-25 12:48:01 +0200

    Nombres actualizados

--- aer456.java
+++ aer456.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class tartita {
+public class aer456 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int n;
