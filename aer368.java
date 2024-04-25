09c8f0ade0d57008d0f9871ecc5edd274ae620b9
David Abellán Lerma <116668339+K0zaky@users.noreply.github.com>
2024-04-25 12:42:13 +0200

    Nombres actualizados

--- aer368.java
+++ aer368.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.lang.Math;
-public class CociendoHuevos {
+public class aer368 {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int huevos=0;
