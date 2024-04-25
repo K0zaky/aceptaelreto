62ccd3cefe6cce9ee316997ec627cd9d9d6cdbbe
David Abellán Lerma <116668339+K0zaky@users.noreply.github.com>
2024-04-25 12:47:13 +0200

    Nombres actualizados

---
+++ aer434.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class aer434 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int num=0;
+        int n=0;
+        int m=0;
+
+        num = reader.nextInt(); reader.nextLine();
+
+        for (int i=1; i<=num; i++){
+
+            n = reader.nextInt();
+            m = reader.nextInt();
+            if (n<m) {
+                System.out.println("ROMANCE");
+            }else if (n==m)
+                System.out.println("ROMANCE"); 
+            else System.out.println("PRINCIPIO");
+        }
+    }
+}
+
