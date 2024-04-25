19217a0082e3048530c0515bed7186a7d883d4aa
David Abellán Lerma <116668339+K0zaky@users.noreply.github.com>
2024-04-25 12:34:32 +0200

    Nombres actualizados

--- aer217.java
+++ aer217.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class decalle {
+public class aer217 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int num=0;
