51cc2eada440cba42b5649f5cf1115a6588097bd
David Abellán Lerma <116668339+K0zaky@users.noreply.github.com>
2024-04-25 12:32:37 +0200

    Nombres actualizados

--- aer337.java
+++ aer337.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-    public class abuMaria {
+    public class aer337 {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
        
