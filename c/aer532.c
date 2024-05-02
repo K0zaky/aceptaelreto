77bce4728e6bf455a8f121afad0294e64d7a997a
David Abellán Lerma <116668339+K0zaky@users.noreply.github.com>
2024-05-02 18:23:53 +0200

    Reduciendo envases en C

--- c/aer532.c
+++ c/aer532.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main() {
     int n;
-    char nums[100];
     int n1, n2;
-    char *token;
 
     scanf("%d", &n);
     for (; n > 0; n--) {
-        scanf("%s", nums);
+        scanf("%d", &n1);
+        scanf("%d", &n2);
 
-        token = strtok(nums, " ");
-        if (token != NULL) {
-            n1 = atoi(token);
-        }
+        int d=n2-n1;
 
-        token = strtok(NULL, " ");
-        if (token != NULL) {
-            n2 = atoi(token);
-        }
-
-        // Restar n1 de n2 para obtener la diferencia correcta
-        int resultado = n1 - n2;
-        printf("%d\n", resultado);
+        printf("%d\n", d);
     }
 
     return 0;
